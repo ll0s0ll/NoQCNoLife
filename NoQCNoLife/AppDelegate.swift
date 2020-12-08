70b17628c62cdefcd114220ca7fb0c98d4f1da04
ShunITO <shunito.s110@gmail.com>
2020-12-09 08:45:04 +0900

    Initial Commit

---
+++ NoQCNoLife/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoQCNoLife
+//
+//  Created by admin on 2020/12/09.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
